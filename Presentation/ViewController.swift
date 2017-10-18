3e015b9d658f5f8fd7934b7804b57f7ebbb1061a
北川真理 <m.kitagawa@planet-table.com>
2017-10-18 20:45:58 +0900

    Initial Commit

---
+++ Presentation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Presentation
+//
+//  Created by 北川真理 on 2017/10/18.
+//  Copyright © 2017年 北川真理. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
