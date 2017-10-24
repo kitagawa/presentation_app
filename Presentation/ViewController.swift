eaa10cdfa7db2d2f3c31bde476fa00f45bbf88fc
北川真理 <m.kitagawa@planet-table.com>
2017-10-24 18:12:01 +0900

    テストアプリ作成

--- Presentation/ViewController.swift
+++ Presentation/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var url:String = "https://google.com/"
+    var targetURL = Bundle.main.path(forResource: "index", ofType: "html");
+    var videoURL = Bundle.main.path(forResource: "animation1017", ofType: "mp4");
+    
+    @IBOutlet weak var webview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webview.frame = self.view.bounds
+        webview.allowsInlineMediaPlayback = true
+        
+        
+//        let htmlString:NSString = NSString(format: "<video id='video' width='300' height='200' src='file://animation1017.mp4' controls autoplay></video>");
+//        
+//        webview.loadHTMLString(htmlString as String, baseURL:nil)
+        let requestURL = NSURL(string: targetURL!)
+        let request = NSURLRequest(url: requestURL! as URL)
+        webview.loadRequest(request as URLRequest)
+//        let videoUrl:NSString = "http://hls-sample.s3-website-ap-northeast-1.amazonaws.com/index.m3u8";
+//        let videoUrl:NSString = "file:://animation1017.mp4";
+//        let videoUrl = Bundle.main.path(forResource: "animation1017", ofType: "mp4");
+//        let htmlString:NSString = NSString(format: "<video width='300' height='200' src='file://%@' controls autoplay playsinline webkit-playsinline></video>",videoUrl!);
+//        let imageUrl = Bundle.main.path(forResource: "icon", ofType: "jpg");
+//        let htmlString:NSString = NSString(format: "<img width='300' height='200' src='file://%@'></img>",imageUrl!);
+//        webview.loadHTMLString(htmlString as String, baseURL: nil);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
